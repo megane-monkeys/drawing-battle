621c6cb9edbfd64ae061a2f4c5cd246cd2dcbe50
y-ksk <yoshi.ksk.1007@gmail.com>
2019-12-31 20:10:21 +0900

    build

---
+++ back/src/build/precache-manifest.a451c06311636551161bc85de3bea323.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eed1acf97845dd6d806fa00cdb0f7ad1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd662638badb79bd894b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e0c033ee3074660b3538",
+    "url": "/static/js/2.3777e3de.chunk.js"
+  },
+  {
+    "revision": "04ed54b8414d9163863054c75c2ff975",
+    "url": "/static/js/2.3777e3de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bd662638badb79bd894b",
+    "url": "/static/js/main.ce11947b.chunk.js"
+  },
+  {
+    "revision": "5d300a419a58d50051e6",
+    "url": "/static/js/runtime-main.947a4f37.js"
+  }
+]);
