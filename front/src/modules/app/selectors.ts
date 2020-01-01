bd54c2f8795324fecc2df41164ba7eed09e29f48
senbeiman <1000bei10000@gmail.com>
2020-01-01 12:33:06 +0900

    リファクタリング

---
+++ front/src/modules/app/selectors.ts
@@ -0,0 +1,5 @@
+import { RootState } from "../reducer";
+
+const appSelectors = (state: RootState) => state.app;
+
+export default appSelectors;
--- front/src/modules/timer/slice.ts
+++ front/src/modules/timer/slice.ts
@@ -1,8 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { TimerStatus } from "../../constants/timerStatus"
 
 const initialState = {
-    state: TimerStatus.INITIAL,
     startTime: new Date().getTime(),
     milliseconds: 0
 };
@@ -11,19 +9,11 @@ const slice = createSlice({
     name: "timer",
     initialState,
     reducers: {
-        abortTimer: (state, action) => {
-            state.state = TimerStatus.ABORT;
-        },
-        startTimer: (state, action) => {
+        restartTimer: (state, action) => {
             state.startTime = new Date().getTime();
-            state.state = TimerStatus.WORKING;
-        },
-        resetTimer: (state, action) => {
-            state.state = TimerStatus.RESETTING;
             state.milliseconds = 0;
         },
         stopTimer: (state, action) => {
-            state.state = TimerStatus.FINISH;
             state.milliseconds = new Date().getTime() - state.startTime;
         },
     }
